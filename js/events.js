7c2d565ec9187e35db0bbca756c34f0572cda365
Boni-amin <boniamin.info@gmail.com>
2021-08-13 20:24:54 +0600

    first commit

---
+++ js/events.js
@@ -0,0 +1,11 @@
+// Button number 2
+function changeBodyColor(){
+    document.body.style.backgroundColor = 'green';
+}
+
+// Button Number 3
+function makeButton (){
+    document.body.style.backgroundColor = 'blue';
+}
+const blueButton = document.getElementById('make-ble-color');
+blueButton.onclick = makeButton; /* Akhane (call) avabe Call kora jabe na  */
